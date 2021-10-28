4fede08eafce2f38204ab295e09a80607e3767ff
Edwin Burelo <michaeledwin@live.com.mx>
2021-10-27 23:40:08 -0500

    update obj json reporteValvula

--- app.js
+++ app.js
@@ -10,11 +10,19 @@ const storageStrategy = multer.memoryStorage()
 const upload  = multer({ storage: storageStrategy })
 
 const app = express();
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 
 const pdfRoute = require('./routes/pdfmake');
 app.use(pdfRoute);
 
+// app.use(bodyParser.json({limit: '50mb'}) );
+// app.use(bodyParser.urlencoded({
+//   limit: "5mb", 
+//   extended: true,
+//   parameterLimit:5000000,
+// }))
+
+// app.use(express.json())
 // app.use(express.static(path.join(__dirname, 'public')));
 // app.use(bodyParser.urlencoded({extended:true}));
 
